ceb59db9c48f4d4ef7f92fdc42975390aac553c4
Ashley Clark <ashleyclarkindy@gmail.com>
2023-05-15 17:06:30 -0400

    Set up React Router

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
+import Search from './pages/search/Search'
+import Park from './pages/park/Park'
+
+const router = createBrowserRouter([
+  { path:"/",
+    element: <Search />,
+  },
+  {
+    path: "ParkName",
+    element: <Park />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- packages/frontend/src/pages/search/Search.jsx
+++ packages/frontend/src/pages/search/Search.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
 import "./search.css";
 
 
@@ -13,45 +13,44 @@ const Search = () => {
     <div className='search-page'>
       <Navbar>
         <Container>
-          <Navbar.Brand href="#home">Search</Navbar.Brand>
+          <Navbar.Brand href="/">Search</Navbar.Brand>
         </Container>
       </Navbar>
-      <Container>
-        <Row>
-          <Col></Col>
-          <Col className='park-search' xs={6}>
-            <h2>Select a Park</h2>
-            <p>Choose on of the following search methods</p>
-            <Form>
-              <div key={`inline-radio`}>
-                <Form.Check
-                  inline
-                  label="Search"
-                  name="group1"
-                  type="radio"
-                  id={`inline-radio-1`}
-                />
-                <Form.Check
-                  inline
-                  label="By State"
-                  name="group1"
-                  type="radio"
-                  id={`inline-radio-2`}
-                />
-                <Form.Check
-                  inline
-                  label="By Activity"
-                  name="group1"
-                  type="radio"
-                  id={`inline-radio-3`}
-                />
-              </div>
-            </Form>
-            <Form.Control type="text"/>
-          </Col>
-          <Col></Col>
-        </Row>
+      <Container className='park-search text-center'>
+        <h2>Select a Park</h2>
+        <p>Choose on of the following search methods</p>
+        <Form>
+          <div key={`inline-radio`}>
+            <Form.Check
+              inline
+              label="Search"
+              name="group1"
+              type="radio"
+              id={`inline-radio-1`}
+            />
+            <Form.Check
+              inline
+              label="By State"
+              name="group1"
+              type="radio"
+              id={`inline-radio-2`}
+            />
+            <Form.Check
+              inline
+              label="By Activity"
+              name="group1"
+              type="radio"
+              id={`inline-radio-3`}
+            />
+          </div>
+        </Form>
+        <Form.Control type="text"/>
       </Container>
+      <Nav>
+        <Nav.Item as="li">
+          <Nav.Link href='ParkName' class='link-secondary'>Temp to Park</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }

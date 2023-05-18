672a08895ad176b31da0527d2fbc118063a7af20
Ashley Clark <ashleyclarkindy@gmail.com>
2023-05-18 17:29:29 -0400

    Began connecting frontend to backend

--- packages/frontend/src/pages/park/Park.jsx
+++ packages/frontend/src/pages/park/Park.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./park.css";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import ParkAlerts from "./PAlert";
+import ParkAlerts from "../../components/PAlert";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Nav from 'react-bootstrap/Nav';
 
 const Park = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_BASE_URL}/park`)
+      .then((res) => res.json())
+      .then((data) => setMessage(data.pName));
+  }, []);
+
   return (
     <div id="park-page">
       <Navbar>
@@ -15,15 +23,15 @@ const Park = () => {
           <Navbar.Brand href="/">Search</Navbar.Brand>
         </Container>
       </Navbar>
-      <h1>Park Name</h1>
+      <h1>{message}</h1>
       <ParkAlerts />
-      <Container>
+      <Container id="description">
         <h3>Description:</h3>
       </Container>
       <Tabs defaultActiveKey="contact" fill>
-        <Tab eventKey="contact" title="Contact Info">Contact Information!</Tab>
-        <Tab eventKey="weather" title="Weather">Weather Information!</Tab>
-        <Tab eventKey="fees" title="Fees/Parking">Fees and Parking Information!</Tab>
+        <Tab className="tabContent" eventKey="contact" title="Contact Info">Contact Information!</Tab>
+        <Tab className="tabContent" eventKey="weather" title="Weather">Weather Information!</Tab>
+        <Tab className="tabContent" eventKey="fees" title="Fees/Parking">Fees and Parking Information!</Tab>
       </Tabs>
       <Nav className='side-nav flex-column'>
         <Nav.Link href="ParkName/hours">Operating Hours</Nav.Link>

5ec66bcce55e98270e730d55ddfb6db46d1c503e
Ashley Clark <ashleyclarkindy@gmail.com>
2023-06-06 16:33:51 -0400

    Started developing Camping page

---
+++ packages/frontend/src/components/amenityTable.jsx
@@ -0,0 +1,79 @@
+import Table from 'react-bootstrap/Table';
+
+const AmenityTable = (data) => {
+  let info = data.data;
+
+  // checking if amenities are present
+  if (info.trashRecyclingCollection === "") {
+    return (
+      <p>No amenities available.</p>
+    )
+  } else {
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <th>Amenity</th>
+            <th>Availability</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Trash & Recycling Collection</td>
+            <td>{info.trashRecyclingCollection}</td>
+          </tr>
+          <tr>
+            <td>Toilets</td>
+            <td>{info.toilets.map((t) => (<div>{t}</div>))}</td>
+          </tr>
+          <tr>
+            <td>Showers</td>
+            <td>{info.showers.map((t) => (<div>{t}</div>))}</td>
+          </tr>
+          <tr>
+            <td>Internet</td>
+            <td>{info.internetConnectivity}</td>
+          </tr>
+          <tr>
+            <td>Cell Phone Reception</td>
+            <td>{info.cellPhoneReception}</td>
+          </tr>
+          <tr>
+            <td>Laundry</td>
+            <td>{info.laundry}</td>
+          </tr>
+          <tr>
+            <td>Amphitheater</td>
+            <td>{info.amphitheater}</td>
+          </tr>
+          <tr>
+            <td>Dump Station</td>
+            <td>{info.dumpStation}</td>
+          </tr>
+          <tr>
+            <td>Camp Store</td>
+            <td>{info.campStore}</td>
+          </tr>
+          <tr>
+            <td>Potable Water</td>
+            <td>{info.potableWater.map((t) => (<div>{t}</div>))}</td>
+          </tr>
+          <tr>
+            <td>Ice for Sale</td>
+            <td>{info.iceAvailableForSale}</td>
+          </tr>
+          <tr>
+            <td>Firewood for Sale</td>
+            <td>{info.firewoodForSale}</td>
+          </tr>
+          <tr>
+            <td>Food Storage Lockers</td>
+            <td>{info.foodStorageLockers}</td>
+          </tr>
+        </tbody>
+      </Table>
+    )
+  }
+}
+
+export default AmenityTable;
--- packages/frontend/src/pages/camping/Camping.jsx
+++ packages/frontend/src/pages/camping/Camping.jsx
@@ -1,29 +1,107 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import "./camping.css";
 import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Table from 'react-bootstrap/Table';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
+import Loading from '../../components/loading/Loading';
+import AmenityTable from '../../components/amenityTable';
+import HoursControl from '../../components/hoursControl';
 
 const Camping = () => {
-  return (
+  const location = useLocation();
+  // pid holds park id
+  const pid = location.state.code;
+
+  const [info, setInfo] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const fetchCamp = async() => {
+    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/camps/${pid}`);
+    const data = await response.json();
+    setInfo(data);
+  }
+
+  useEffect(() => {
+    fetchCamp().then(() => setLoading(false))
+  }, []);
+
+  return loading ? <Loading /> : (
     <div>
-      <Tab.Container defaultActiveKey="first">
-        <Nav variant='pills'>
-          <Nav.Item>
-            <Nav.Link eventKey='first'>Camp Ground 1</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey='second'>Camp Ground 2</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Tab.Content>
-          <Tab.Pane eventKey='first'>
-            Info
-          </Tab.Pane>
-          <Tab.Pane eventKey='second'>Info 2</Tab.Pane>
-        </Tab.Content>
-      </Tab.Container>
+      <Navbar id='park-nav' sticky='top'>
+        <Container>
+          <Navbar.Brand href="/">Search</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Tabs defaultActiveKey={info[0].name} id="cTabs">
+        {info.map((entry) => (
+          <Tab className='tabContent' eventKey={entry.name} title={entry.name}>
+            <p>{entry.description}</p>
+            <h4>Hours</h4>
+            {entry.operatingHours.map((ent) => (
+              <HoursControl data={ent} />
+            ))}
+            <h4>Reservations</h4>
+            <p>{entry.reservationInfo}</p>
+            <a href={entry.reservationUrl}>Make a Reservation</a>
+            <h4>Amenities</h4>
+            <AmenityTable data={entry.amenities}/>
+            <h4>Fees</h4>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Fee</th>
+                  <th>Cost</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {entry.fees.map((f) => (
+                  <tr>
+                    <td>{f.title}</td>
+                    <td>${f.cost}</td>
+                    <td>{f.description}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+            <h4>Campsites</h4>
+            <ListGroup>
+              <ListGroup.Item>Total Sites: {entry.campsites.totalSites}</ListGroup.Item>
+              <ListGroup.Item>Electrical Hookup: {entry.campsites.electricalHookups}</ListGroup.Item>
+              <ListGroup.Item>RV Only: {entry.campsites.rvOnly}</ListGroup.Item>
+              <ListGroup.Item>Tent Only: {entry.campsites.tentOnly}</ListGroup.Item>
+              <ListGroup.Item>Horse: {entry.campsites.horse}</ListGroup.Item>
+              <ListGroup.Item>Walk or Boat to: {entry.campsites.walkBoatTo}</ListGroup.Item>
+              <ListGroup.Item>Group: {entry.campsites.group}</ListGroup.Item>
+              <ListGroup.Item>Other: {entry.campsites.other}</ListGroup.Item>
+            </ListGroup>
+            <h4>Additional Info</h4>
+            <p>{entry.regulationsOverview}</p>
+            <h5>Accessability</h5>
+            <ul>
+              <li>{entry.accessibility.wheelchairAccess}</li>
+              <li>{entry.accessibility.adaInfo}</li>
+            </ul>
+            <h5>RVs & Trailers</h5>
+            <ul>
+              <li>RV max length: {entry.accessibility.rvMaxLength}</li>
+              <li>Trailer max length: {entry.accessibility.trailerMaxLength}</li>
+              <li>{entry.accessibility.rvInfo}</li>
+            </ul>
+            <h5>Other</h5>
+            <p>{entry.accessibility.cellPhoneInfo} {entry.accessibility.internetInfo}</p>
+            <p>{entry.accessibility.fireStovePolicy}</p>
+            <p>{entry.accessibility.additionalInfo}</p>
+            <p>{entry.accessibility.accessRoads}</p>
+          </Tab>
+        ))}
+      </Tabs>
     </div>
   );
 }

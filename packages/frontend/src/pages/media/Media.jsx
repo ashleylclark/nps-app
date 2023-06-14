1cdcbd9cead061d3231adad5814331554f6f7379
Ashley Clark <ashleyclarkindy@gmail.com>
2023-06-14 15:11:34 -0400

    Fixed Media page design and warnings

--- packages/frontend/src/pages/media/Media.jsx
+++ packages/frontend/src/pages/media/Media.jsx
@@ -3,10 +3,7 @@ import "./media.css";
 import { useLocation } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import Loading from '../../components/loading/Loading';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
@@ -38,7 +35,7 @@ const Media = () => {
         <Card>
           <Card.Img variant='top' src={g.images[0].url} alt={g.images[0].altText} />
           <Card.Body>
-            <Card.Title><a href={g.url}>{g.title}</a></Card.Title>
+            <Card.Title><a className='tile-a' href={g.url}>{g.title}</a></Card.Title>
             <Card.Text>{g.description}</Card.Text>
           </Card.Body>
         </Card>
@@ -50,44 +47,41 @@ const Media = () => {
     if (web.images.length) {
       return (
         <Card.Img src={web.images[0].url} alt={web.images[0].altText} />
-        // <img src={web.images[0].url} alt={web.images[0].altText} style={{height:200}}/>
       );
     }
   }
 
   return loading ? <Loading /> : (
-    <div>
+    <div id='media-pg'>
       <Navbar id='park-nav'>
         <Container>
           <Navbar.Brand href="/">Search</Navbar.Brand>
         </Container>
       </Navbar>
-      <h2>Galleries</h2>
-      <Masonry columnsCount={4} gutter='10px'>
-        {img.map((gall) => (
-          <>{checkContraints(gall)}</>
+      <h2 className='m-head'>Galleries</h2>
+      <Masonry className='img-tiles' columnsCount={4} gutter='20px'>
+        {img.map((gall, i) => (
+          <div key={i}>{checkContraints(gall)}</div>
         ))}
       </Masonry>
-      <hr></hr>
-      <h2>Webcams</h2>
-      <Masonry columnsCount={4} gutter='10px'>
-        {cam.map((wb) => (
-          <Card>
+      <h2 className='m-head'>Webcams</h2>
+      <Masonry className='img-tiles' columnsCount={4} gutter='20px'>
+        {cam.map((wb, i) => (
+          <Card key={i}>
             <Card.Body>
-              <Card.Title><a href={wb.url}>{wb.title}</a></Card.Title>
+              <Card.Title><a className='tile-a' href={wb.url}>{wb.title}</a></Card.Title>
               <Card.Text>{wb.description}</Card.Text>
             </Card.Body>
             {isImage(wb)}
           </Card>
         ))}
       </Masonry>
-      <hr></hr>
-      <h2>Videos</h2>
-      <Masonry columnsCount={4} gutter='10px'>
-        {vid.map((v) => (
-          <Card>
+      <h2 className='m-head'>Videos</h2>
+      <Masonry className='img-tiles' columnsCount={4} gutter='20px'>
+        {vid.map((v, i) => (
+          <Card key={i}>
             <Card.Body>
-              <Card.Title><a href={v.permalinkUrl}>{v.title}</a></Card.Title>
+              <Card.Title><a className='tile-a' href={v.permalinkUrl}>{v.title}</a></Card.Title>
               <Card.Text>{v.description}</Card.Text>
             </Card.Body>
           </Card>

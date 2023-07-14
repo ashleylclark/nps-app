fce3ec0578f1ff55431cc188bb9990a1e90a1fca
Ashley Clark <ashleyclarkindy@gmail.com>
2023-07-14 16:20:03 -0400

    added support for no info

--- packages/frontend/src/pages/camping/Camping.jsx
+++ packages/frontend/src/pages/camping/Camping.jsx
@@ -3,6 +3,7 @@ import { useLocation, Link } from 'react-router-dom';
 import Loading from '../../components/loading/Loading';
 import AmenityTable from '../../components/amenityTable';
 import HoursControl from '../../components/hoursControl';
+import { displayNoInfo } from '../../components/noInfo/noInfo';
 
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
@@ -14,6 +15,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "./camping.css";
+import '../../components/noInfo/noInfo.css';
 
 const Camping = () => {
   const location = useLocation();
@@ -33,25 +35,8 @@ const Camping = () => {
     fetchCamp().then(() => setLoading(false))
   }, []);
 
-  return loading ? <Loading /> : (
-    <div id='camp-pg'>
-      <Navbar id='park-nav' sticky='top'>
-        <Container className='nav-container'>
-          <Navbar.Brand href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-          </Navbar.Brand>
-          <Navbar.Collapse>
-            <Nav className='me-auto'>
-              <Link className='pLinks' to={location.pathname + "/../"}>Park</Link>
-              <Link className='pLinks' to={location.pathname + "/../hours"} state={location.state}>Hours</Link>
-              <Link className='pLinks' to={location.pathname + "/../media"} state={location.state}>Media</Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
+  const checkInfo = (info) => {
+    return info.length ? (
       <Tab.Container defaultActiveKey={info[0].name}>
         <Row id='camp-cont'>
           <Col sm={2} id='camp-nav'>
@@ -76,7 +61,7 @@ const Camping = () => {
                     ))}
                   </Row>
                   <Row>
-                    <Col>
+                    <Col sm='12' lg='6'>
                       <h3 className='c-headers'>Amenities</h3>
                       <AmenityTable data={entry.amenities}/>
                     </Col>
@@ -142,80 +127,28 @@ const Camping = () => {
           </Col>
         </Row>
       </Tab.Container>
-        {/* {info.map((entry, i) => (
-          <Tab className='tabContent' eventKey={entry.name} title={entry.name} key={i}>
-            <p className='camp-p'>{entry.description}</p>
-            <h3 className='c-headers'>Hours</h3>
-            <Row className='camp-hrs' md={2}>
-              {entry.operatingHours.map((ent, i) => (
-                <Col className='c-hrs' key={i}>
-                  <HoursControl data={ent} />
-                </Col>
-              ))}
-            </Row>
-            <Row>
-              <Col>
-                <h3 className='c-headers'>Amenities</h3>
-                <AmenityTable data={entry.amenities}/>
-              </Col>
-              <Col>
-                <h3 className='c-headers'>Reservations</h3>
-                <p id="reserv">{entry.reservationInfo}</p>
-                <a id='mkres' href={entry.reservationUrl}>Make a Reservation</a>
-              </Col>
-            </Row>
-            <h3 className='c-headers'>Fees</h3>
-            <Table id='c-fees' bordered>
-              <thead>
-                <tr>
-                  <th>Fee</th>
-                  <th>Cost</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {entry.fees.map((f, i) => (
-                  <tr key={i}>
-                    <td>{f.title}</td>
-                    <td>${f.cost}</td>
-                    <td>{f.description}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-            <h3 className='c-headers'>Campsites</h3>
-            <ListGroup id='campsites'>
-              <ListGroup.Item className='c-info'>Total Sites: {entry.campsites.totalSites}</ListGroup.Item>
-              <ListGroup.Item className='c-info'>Electrical Hookup: {entry.campsites.electricalHookups}</ListGroup.Item>
-              <ListGroup.Item className='c-info'>RV Only: {entry.campsites.rvOnly}</ListGroup.Item>
-              <ListGroup.Item className='c-info'>Tent Only: {entry.campsites.tentOnly}</ListGroup.Item>
-              <ListGroup.Item className='c-info'>Horse: {entry.campsites.horse}</ListGroup.Item>
-              <ListGroup.Item className='c-info'>Walk or Boat to: {entry.campsites.walkBoatTo}</ListGroup.Item>
-              <ListGroup.Item className='c-info'>Group: {entry.campsites.group}</ListGroup.Item>
-              <ListGroup.Item className='c-info'>Other: {entry.campsites.other}</ListGroup.Item>
-            </ListGroup>
-            <h3 className='c-headers'>Additional Info</h3>
-            <div id='add-info'>
-              <p className="camp-p">{entry.regulationsOverview}</p>
-              <h4>Accessability</h4>
-              <ul>
-                <li className='camp-li'>{entry.accessibility.wheelchairAccess}</li>
-                <li className='camp-li'>{entry.accessibility.adaInfo}</li>
-              </ul>
-              <h4>RVs & Trailers</h4>
-              <ul>
-                <li className='camp-li'>RV max length: {entry.accessibility.rvMaxLength}</li>
-                <li className='camp-li'>Trailer max length: {entry.accessibility.trailerMaxLength}</li>
-                <li className='camp-li'>{entry.accessibility.rvInfo}</li>
-              </ul>
-              <h4>Other</h4>
-              <p>{entry.accessibility.cellPhoneInfo} {entry.accessibility.internetInfo}</p>
-              <p>{entry.accessibility.fireStovePolicy}</p>
-              <p>{entry.accessibility.additionalInfo}</p>
-              <p>{entry.accessibility.accessRoads}</p>
-            </div>
-          </Tab>
-        ))} */}
+    ) : displayNoInfo('camping')
+  }
+
+  return loading ? <Loading /> : (
+    <div id='camp-pg'>
+      <Navbar id='park-nav' fixed='top'>
+        <Container className='nav-container'>
+          <Navbar.Brand href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+          </Navbar.Brand>
+          <Navbar.Collapse>
+            <Nav className='me-auto'>
+              <Link className='pLinks' to={location.pathname + "/../"}>Park</Link>
+              <Link className='pLinks' to={location.pathname + "/../hours"} state={location.state}>Hours</Link>
+              <Link className='pLinks' to={location.pathname + "/../media"} state={location.state}>Media</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {checkInfo(info)}
     </div>
   );
 }

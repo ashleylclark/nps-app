862ee0649238abb03a95affc6635223abeb727f6
Ashley Clark <ashleyclarkindy@gmail.com>
2023-07-18 15:08:35 -0400

    Updating styling

--- packages/frontend/src/pages/search/Search.jsx
+++ packages/frontend/src/pages/search/Search.jsx
@@ -181,12 +181,17 @@ const Search = () => {
       <Navbar id='searchNav'>
         <Container className='nav-container'>
           <Navbar.Brand href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
+            <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
+          </svg>
           </Navbar.Brand>
         </Container>
       </Navbar>
+      <div id='search-title'>
+        <h1 className='s-t'>Discover Nature:</h1>
+        <h1 className='s-t'>Explore the National Parks!</h1>
+      </div>
       <Container id="park-search" className='text-center'>
         <h2>Select a Park</h2>
         <p>Choose one of the following search methods.</p>
